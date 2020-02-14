dc5577a66f804009d544c4afc1f56423aaf86f48
owencsmith <ocsmith@wpi.edu>
2020-02-14 18:56:38 -0500

    refactored package name to requirement

--- src/main/java/edu/wpi/cs3733/c20/teamS/App.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/App.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.cs3733.c20.teamS;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.cs3733.c20.teamS;
 
 public class Main {
 
