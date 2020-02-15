b93fe5b193e8e2fd9cc4ca7224dabcffacf839a7
Newell Clark <newellofthefuture@gmail.com>
2020-02-15 18:34:34 -0500

    Created skeletons for Employee, ServiceRequest, and various enumeration types.

---
+++ ReloadedAndMobilized/ReloadedAndMobilized/ServiceRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquishySquidward.ReloadedAndMobilized
+{
+    public enum ServiceKind
+    {
+        Janitor
+    }
+    public enum ServiceStatus
+    {
+        Created,
+        Assigned, 
+        Completed
+    }
+
+    public class ServiceRequest
+    {
+        public ServiceKind Kind { get; }
+        public Employee Employee { get; }
+        public ServiceStatus Status => throw new NotImplementedException();
+
+    }
+}
