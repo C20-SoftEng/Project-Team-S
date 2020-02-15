9c5eee097f2fcd5db0cc7bb4ca4a7cc4ea6f50f1
Mario Castro <33728305+CluelessTimeTraveler@users.noreply.github.com>
2020-02-15 17:31:24 -0500

    Update Main.java

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,7 +5,7 @@ import edu.wpi.teamname.database.DatabaseController;
 public class Main {
 
   public static void main(String[] args) {
-    //App.launch(App.class, args);
+    App.launch(App.class, args);
     DatabaseController dbCont = new DatabaseController();
   }
 }
