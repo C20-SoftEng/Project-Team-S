52b07279793d54af9fc44c1e44a8aff720f7c94d
Jordan <jstoessel@wpi.edu>
2020-02-15 20:23:45 -0500

    Connected UI_client.fxml to main

--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -1,10 +1,16 @@
 package edu.wpi.cs3733.c20.teamS;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        App.launch(Main.class, args);
+    }
 
 import com.sun.javafx.geom.Edge;
 import edu.wpi.cs3733.c20.teamS.database.DataClasses.EdgeData;
@@ -66,6 +72,10 @@ public class Main {
     //dbCont.addNode(new NodeData("Hi",1,2,1,"Fuller","YESD","LONGG","SHORTT"));
   }
 
+    @Override
+    public void start(Stage primaryStage) {
+        mainToLoginScreen test = new mainToLoginScreen(primaryStage);
+    }
 
  // mainToLoginScreen test = new mainToLoginScreen(primaryStage);
 
