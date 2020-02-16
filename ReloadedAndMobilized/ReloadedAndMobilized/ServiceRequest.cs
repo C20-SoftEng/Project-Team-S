3e8307289bb0db4a19940922ff946fd838bc81c5
Newell Clark <newellofthefuture@gmail.com>
2020-02-15 20:00:52 -0500

    .

--- ReloadedAndMobilized/ReloadedAndMobilized/ServiceRequest.cs
+++ ReloadedAndMobilized/ReloadedAndMobilized/ServiceRequest.cs
@@ -18,33 +18,9 @@ namespace SquishySquidward.ReloadedAndMobilized
 
     public class ServiceRequest
     {
-        public ServiceRequest(ServiceKind kind) : this(kind, DateTime.UtcNow) { }
-        public ServiceRequest(ServiceKind kind, DateTime dateCreated)
-        {
-            this.Kind = kind;
-            this.DateCreated = dateCreated;
-            this.Status = ServiceStatus.Created;
-        }
-        public int? ID { get; }
-        public DateTime DateCreated { get; }
         public ServiceKind Kind { get; }
-        public Employee Employee { get; private set; }
-        public ServiceStatus Status { get; private set; }
-        public void Assign(Employee employee)
-        {
-            if (employee is null) throw new ArgumentNullException(nameof(employee));
+        public Employee Employee { get; }
+        public ServiceStatus Status => throw new NotImplementedException();
 
-            Employee = employee;
-            Status = ServiceStatus.Assigned;
-        }
-        public void Complete()
-        {
-            Status = ServiceStatus.Completed;
-        }
-        public void Cancel()
-        {
-            Status = ServiceStatus.Canceled;
-            Employee = null;
-        }
     }
 }
