f8558badd1b5f2141f6445fd97bfd7a61e0cf5d6
Tommy <tommykim1998@yahoo.com>
2020-02-16 20:35:44 -0500

    added new floor renders

--- src/main/java/edu/wpi/cs3733/c20/teamS/mainScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/mainScreenController.java
@@ -1,83 +1,73 @@
 package edu.wpi.cs3733.c20.teamS;
-
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
 public class mainScreenController implements Initializable {
-
-private MapZoomer zoomer;
-@FXML private ImageView mapImage;
-@FXML private ScrollPane scrollPane;
-
-@FXML private void onZoomInClicked(ActionEvent event){
-        @FXML
-        private ImageView mapImage;
-
-        @FXML private ScrollPane mapPane;
-
-    @FXML
-        void floorClicked1(ActionEvent event) {
-
-        }
-
-        @FXML
-        void floorClicked2(ActionEvent event) {
-
-        }
-
-        @FXML
-        void floorClicked3(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onDownClicked(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onFloorClicked4(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onFloorClicked5(ActionEvent event) {
-
-        }
-    public mainScreenController() {
-    }
-
+    final double SCALE_DELTA = 1.1;
+    public double SCALE_TOTAL = 1;
+    private MapZoomer zoomer;
+    Image floor1 = new Image("images/Floors/hospital_1st_floor.png");
+    Image floor2 = new Image("images/Floors/hospital_2nd_floor.png");
+    Image floor3 = new Image("images/Floors/hospital_3rd_floor.png");
+    Image floor4 = new Image("images/Floors/hospital_4th_floor.png");
+    Image floor5 = new Image("images/Floors/hospital_5th_floor.png");
     @FXML
     private ImageView mapImage;
-
-        @FXML
-        void onHelpClicked(ActionEvent event) {
-
-        }
-
-    @FXML private void onZoomInClicked(ActionEvent event){
-
-    zoomer.zoomIn();
-
-}
-@FXML private void onZoomOutClicked(ActionEvent event){
-
-    zoomer.zoomOut();
-}
-
-@Override
-public void initialize(URL location, ResourceBundle resources) {
-    zoomer = new MapZoomer(mapImage, scrollPane);
-}
+    @FXML
+    private ScrollPane mapPane;
+    @FXML
+    void floorClicked1(ActionEvent event) {
+        mapImage.setImage(floor1);
+    }
+    @FXML
+    void floorClicked2(ActionEvent event) {
+        mapImage.setImage(floor2);
+    }
+    @FXML
+    void floorClicked3(ActionEvent event) {
+        mapImage.setImage(floor3);
+    }
+    @FXML
+    void onFloorClicked4(ActionEvent event) {
+        mapImage.setImage(floor4);
+    }
+    @FXML
+    void onFloorClicked5(ActionEvent event) {
+        mapImage.setImage(floor5);
+    }
+    @FXML
+    void onUpClicked(ActionEvent event) {
+        mapImage.setImage(new Image("images/Floors/hospital_4th_floor.png"));
+    }
+    @FXML
+    void onDownClicked(ActionEvent event) {
+        mapImage.setImage(new Image("images/Floors/hospital_4th_floor.png"));
+    }
+    @FXML
+    void onHelpClicked(ActionEvent event) {
+    }
+    @FXML
+    void onStaffClicked(ActionEvent event) {
+    }
+    @FXML
+    void onZoomInClicked(ActionEvent event) {
+        this.zoomer.zoomIn();
+    }
+    @FXML
+    void onZoomOutClicked(ActionEvent event) {
+        Node content = mapPane.getContent();
+        this.zoomer.zoomOut();
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ScrollPane scrollPane;
         zoomer = new MapZoomer(mapImage, scrollPane);
     }
-
 }
