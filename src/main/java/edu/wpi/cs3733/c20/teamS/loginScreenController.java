6d93ebb8608b8f5cf118d56d8b64aa13b1987963
Jordan <jstoessel@wpi.edu>
2020-02-16 22:40:42 -0500

    Added modality to the login screen

---
+++ src/main/java/edu/wpi/cs3733/c20/teamS/loginScreenController.java
@@ -0,0 +1,31 @@
+package edu.wpi.cs3733.c20.teamS;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class loginScreenController {
+
+    @FXML private JFXTextField id;
+
+    @FXML private JFXTextField pw;
+
+    @FXML private JFXButton cancel;
+
+    @FXML private JFXButton enter;
+
+    @FXML private Label wrongID;
+
+    @FXML private Label wrongPW;
+
+    @FXML void onCancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML void onEnterClicked(ActionEvent event) {
+
+    }
+
+}
