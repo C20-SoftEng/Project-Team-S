3d15963a4a71996ea092ce9adb39db00b9a04afc
Newell Clark <newellofthefuture@gmail.com>
2020-02-16 19:29:34 -0500

    Fixing image zoom WIP

--- src/main/java/edu/wpi/cs3733/c20/teamS/MapZoomer.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MapZoomer.java
@@ -5,39 +5,51 @@ import javafx.scene.image.ImageView;
 
 public class MapZoomer {
     private final ImageView mapView;
+    private final ScrollPane scrollPane;
     private int zoomStage;
     private static final int minZoomStage = -2;
     private static final int maxZoomStage = 3;
 
-
-    public MapZoomer(ImageView mapView) {
+    public MapZoomer(ImageView mapView, ScrollPane scrollPane) {
         this.mapView = mapView;
+        this.scrollPane = scrollPane;
     }
 
-
-    private double zoomFactor(){
-       double result = 1.0 + 0.5 * Math.abs(zoomStage);
-       if (zoomStage < 0){
-                return 1.0/result;
-       }
-
-        return result;
-    }
-    private void updateImageSize(){
-        this.mapView.setScaleX(zoomFactor());
-        this.mapView.setScaleY(zoomFactor());
-    }
-    public void zoomIn(){
+    public void zoomIn() {
         if (zoomStage < maxZoomStage){
             zoomStage++;
             updateImageSize();
+            updateBounds();
         }
     }
 
-    public void zoomOut(){
+    public void zoomOut() {
         if (zoomStage > minZoomStage){
             zoomStage--;
             updateImageSize();
+            updateBounds();
         };
+    }
+
+    private double zoomFactor() {
+        double result = 1.0 + 0.5 * Math.abs(zoomStage);
+        if (zoomStage < 0){
+            return 1.0/result;
+        }
+
+        return result;
+    }
+    private void updateImageSize() {
+        this.mapView.setScaleX(zoomFactor());
+        this.mapView.setScaleY(zoomFactor());
+    }
+    private void updateBounds() {
+        double rangeSize = zoomFactor();
+        double minRange = 0;
+        double maxRange = rangeSize;
+        scrollPane.setHmin(minRange);
+        scrollPane.setVmin(minRange);
+        scrollPane.setHmax(maxRange);
+        scrollPane.setVmax(maxRange);
     }
 }
--- src/main/java/edu/wpi/cs3733/c20/teamS/mainScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/mainScreenController.java
@@ -1,10 +1,8 @@
 package edu.wpi.cs3733.c20.teamS;
 
-import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 
@@ -12,75 +10,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class mainScreenController implements Initializable {
-    final double SCALE_DELTA = 1.1;
-    public double SCALE_TOTAL = 1;
+
     private MapZoomer zoomer;
+    @FXML private ImageView mapImage;
+    @FXML private ScrollPane scrollPane;
 
-        @FXML
-        private ImageView mapImage;
-
-        @FXML private ScrollPane mapPane;
-
-    @FXML
-        void floorClicked1(ActionEvent event) {
-
-        }
-
-        @FXML
-        void floorClicked2(ActionEvent event) {
-
-        }
-
-        @FXML
-        void floorClicked3(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onDownClicked(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onFloorClicked4(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onFloorClicked5(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onHelpClicked(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onUpClicked(ActionEvent event) {
-
-        }
-
-    @FXML
-    void onStaffClicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onZoomInClicked(ActionEvent event){
+    @FXML private void onZoomInClicked(ActionEvent event){
 
         this.zoomer.zoomIn();
 
     }
+    @FXML private void onZoomOutClicked(ActionEvent event){
 
-    @FXML void onZoomOutClicked(ActionEvent event){
-        Node content = mapPane.getContent();
-        this.zoomer.zoomOut();
-
+        zoomer.zoomOut();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        zoomer = new MapZoomer(mapImage);
+        zoomer = new MapZoomer(mapImage, scrollPane);
     }
 }
