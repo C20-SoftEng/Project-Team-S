fc72fcf312623b080d62671a0dfed11188e31837
owencsmith <ocsmith@wpi.edu>
2020-02-16 22:28:57 -0500

    fixed conflicts to merge into development

--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -2,19 +2,25 @@ package edu.wpi.cs3733.c20.teamS;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        App.launch(Main.class, args);
-    }
 
-    @Override
-
-    public void start(Stage primaryStage) {
-        mainToLoginScreen test = new mainToLoginScreen(primaryStage);
+  public void start(Stage primaryStage) {
+    try {
+      AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/FXML/UI_client.fxml"));
+      Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+      // scene.getStylesheets().add(getClass().getResource("").toExternalForm());
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
+  public static void main(String[] args) {
+    App.launch();
+  }
 }
