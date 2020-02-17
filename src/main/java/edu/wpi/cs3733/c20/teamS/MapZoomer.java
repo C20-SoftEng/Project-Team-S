bd7922eb3a218d6a74013e28a203c84c208cad39
Jordan <jstoessel@wpi.edu>
2020-02-16 20:38:38 -0500

    Added the Floors

--- src/main/java/edu/wpi/cs3733/c20/teamS/MapZoomer.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MapZoomer.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 
 public class MapZoomer {
     private final ImageView mapView;
-    private final ScrollPane scrollPane;
+    private ScrollPane scrollPane;
     private int zoomStage;
     private static final int minZoomStage = -2;
     private static final int maxZoomStage = 3;
@@ -15,41 +15,40 @@ public class MapZoomer {
         this.scrollPane = scrollPane;
     }
 
-    public void zoomIn() {
-        if (zoomStage < maxZoomStage){
-            zoomStage++;
-            updateImageSize();
-            updateBounds();
-        }
-    }
-
-    public void zoomOut() {
-        if (zoomStage > minZoomStage){
-            zoomStage--;
-            updateImageSize();
-            updateBounds();
-        };
-    }
-
     private double zoomFactor() {
         double result = 1.0 + 0.5 * Math.abs(zoomStage);
-        if (zoomStage < 0){
-            return 1.0/result;
+        if (zoomStage < 0) {
+            return 1.0 / result;
         }
 
         return result;
     }
+
     private void updateImageSize() {
         this.mapView.setScaleX(zoomFactor());
         this.mapView.setScaleY(zoomFactor());
     }
+
+    public void zoomIn() {
+        if (zoomStage < maxZoomStage) {
+            zoomStage++;
+            updateImageSize();
+        }
+    }
+
+    public void zoomOut() {
+        if (zoomStage > minZoomStage) {
+            zoomStage--;
+            updateImageSize();
+        }
+        ;
+    }
+
     private void updateBounds() {
+
         double rangeSize = zoomFactor();
         double minRange = 0;
         double maxRange = rangeSize;
-        scrollPane.setHmin(minRange);
-        scrollPane.setVmin(minRange);
-        scrollPane.setHmax(maxRange);
-        scrollPane.setVmax(maxRange);
+
     }
 }
