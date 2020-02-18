33e2e811b96df8fbf3f45e87b015e39201da5d40
owencsmith <ocsmith@wpi.edu>
2020-02-17 21:35:11 -0500

    new services template

--- src/main/java/edu/wpi/cs3733/c20/teamS/serviceRequests/ServiceVisitor.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/serviceRequests/ServiceVisitor.java
@@ -3,4 +3,5 @@ package edu.wpi.cs3733.c20.teamS.serviceRequests;
 public abstract class ServiceVisitor {
     public abstract void visit(JanitorServiceRequest request);
     public abstract void visit(RideServiceRequest request);
+    public abstract void visit(DrugServiceRequest request);
 }
