b1569e49ee00cffbeb4db23222f6e312ea0e11d2
owencsmith <ocsmith@wpi.edu>
2020-02-18 14:22:33 -0500

    small change, switching back to development to figure out jar issue

--- src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/DrugRequestController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/DrugRequestController.java
@@ -2,9 +2,23 @@ package edu.wpi.cs3733.c20.teamS.app.serviceRequests;
 
 import edu.wpi.cs3733.c20.teamS.app.DialogEvent;
 import edu.wpi.cs3733.c20.teamS.serviceRequests.DrugServiceRequest;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.RideServiceRequest;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.subjects.PublishSubject;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+
 
 public class DrugRequestController {
 
     private final PublishSubject<DialogEvent<DrugServiceRequest>> dialogCompleted_ = PublishSubject.create();
+
+    @FXML
+    private Button okButton;
+
+    public Observable<DialogEvent<DrugServiceRequest>> dialogCompleted() {
+        return dialogCompleted_;
+    }
+
+
 }
