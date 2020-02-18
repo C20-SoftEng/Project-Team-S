c9f484697fc295c520e607fd7a571cc4c3d7e993
amyorozco <aforozco@wpi.edu>
2020-02-17 23:04:06 -0500

    worked on written instructions

--- src/main/java/edu/wpi/cs3733/c20/teamS/pathfinding/WrittenInstructions.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/pathfinding/WrittenInstructions.java
@@ -2,10 +2,13 @@ package edu.wpi.cs3733.c20.teamS.pathfinding;
 
 import com.google.common.graph.MutableGraph;
 import edu.wpi.cs3733.c20.teamS.NodeData;
+import javafx.scene.Node;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.acos;
 
 public class WrittenInstructions {
     double realLifeMeasurementFt = 594.51;
@@ -15,57 +18,56 @@ public class WrittenInstructions {
     double mRatio = realLifeMeasurementM / pixelMeasurement;
 
     ArrayList<NodeData> path;
+    ArrayList<String> instructions = new ArrayList<String>();
 
     public WrittenInstructions(ArrayList<NodeData> path) {
         this.path = path;
     }
-//    double distanceToGoXFT = convert(0, path.get(0).x(), ftRatio);
-//    double distanceToGoYFT = convert(0, path.get(0).y(), ftRatio);
-//    double distanceToGoXM = convert(0, path.get(0).x(), mRatio);
-//    double distanceToGoYM = convert(0, path.get(0).y(), mRatio);
-    public String directions() {
 
-        for (int i = 0; i < path.size(); i++) {
-            double distanceToGoXFT = abs(convert(path.get(0).x(), path.get(i+1).x(), ftRatio));
-            double distanceToGoYFT = abs(convert(path.get(0).y(), path.get(i+1).y(), ftRatio));
-            double distanceToGoXM = abs(convert(path.get(0).x(), path.get(i+1).x(), mRatio));
-            double distanceToGoYM = abs(convert(path.get(0).y(), path.get(i+1).y(), mRatio));
-            if (getAngle(path.get(i), path.get(i + 2)) < -80) {
-//                distanceToGoXFT =+ convert(path.get(i+1).x(),path.get(i+2).x(),ftRatio );
-//                distanceToGoYFT =+ convert(path.get(i+1).y(), path.get(i+2).y(), ftRatio);
-//                distanceToGoXM = +convert(path.get(i + 1).x(), path.get(i + 2).x(), mRatio);
-//                distanceToGoYM = +convert(path.get(i + 1).y(), path.get(i + 2).y(), mRatio);
 
-                if ((convert(path.get(0).y(), path.get(1).y(), ftRatio) < (convert(path.get(0).x(), path.get(1).x(), ftRatio)))){
-                    return ("Turn Right In " + (int) distanceToGoYFT + "FT OR " + (int) distanceToGoYM + "M");
 
-                } else {
-                    return ("Turn Right In " + (int) distanceToGoXFT + "FT OR " + (int) distanceToGoXM + "M");
+    public ArrayList<String> directions() {
+        if (path.size() > 2) {
+            for (int i = 0; i < path.size()-2; i++) {
+
+                if (getAngle(path.get(i), path.get(i + 1), path.get(i + 2)) > 90) {
+                        instructions. add(("Turn Right In " +  distance(path.get(i),
+                                path.get(i+1)) * ftRatio + "FT OR " +
+                                 distance(path.get(i), path.get(i+1)) * mRatio + "M"));
+                    }
+
+                if (abs(getAngle(path.get(i), path.get(i + 1), path.get(i + 2))) > 0) {
+                    instructions. add(("Turn Left In " + ( distance(path.get(i),
+                            path.get(i+1)) * ftRatio + "FT OR " +
+                             distance(path.get(i), path.get(i+1)) * mRatio + "M")));
+                    }
+                else{
+                    instructions. add(("Go Straight For " +  distance(path.get(i),
+                            path.get(i+1)) * ftRatio + "FT OR " +
+                             distance(path.get(i), path.get(i+1)) * mRatio + "M"));
                 }
-
-            }
-
-            if(abs(getAngle(path.get(i), path.get(i + 2)) )> 50){
-//                double angle = getAngle(path.get(i), path.get(i + 2));
-//                double distanceToGoXFT = abs(convert(path.get(0).x(), path.get(i + 1).x(), ftRatio));
-//                double distanceToGoYFT = abs(convert(path.get(0).y(), path.get(i + 1).y(), ftRatio));
-//                double distanceToGoXM = abs(convert(path.get(0).x(), path.get(i + 1).x(), mRatio));
-//                double distanceToGoYM = abs(convert(path.get(0).y(), path.get(i + 1).y(), mRatio));
-                if ((convert(path.get(0).y(), path.get(1).y(), ftRatio) > (convert(path.get(0).x(), path.get(1).x(), ftRatio)))){
-
-                    return ("Turn Left In " + (int) distanceToGoYFT + "FT OR " + (int) distanceToGoYM + "M");
-                } else {
-                    return ("Turn Left In "  + (int) distanceToGoXFT + "FT OR " + (int) distanceToGoXM + "M");
+                if(i == path.size()-2){
+                    return instructions;
                 }
             }
         }
-        return "1";
+
+        return instructions;
     }
 
 
 
-    public double getAngle(NodeData start,NodeData end){
-        double angle = Math.toDegrees(Math.atan2(end.y()-start.y(), end.x() - start.x()));
+    public double distance(NodeData nodeOne, NodeData nodeTwo){
+        //return Math.sqrt(Math.pow((nodeOne.x()-nodeTwo.x()),2)+ Math.pow((nodeOne.y()-nodeTwo.y()),2));
+        return  Math.sqrt(Math.pow((nodeOne.x()-nodeTwo.x()),2)+ Math.pow((nodeOne.y()-nodeTwo.y()),2));
+
+
+    }
+
+    public double getAngle(NodeData one, NodeData two, NodeData three){
+
+        double angle = Math.acos((Math.pow(distance(one,two),2)-Math.pow(distance(two,three),2) - Math.pow(distance(one,three),2))/
+                (-2*distance(two,three)*distance(one,three)));
         return angle;
     }
 
--- src/test/java/edu/wpi/cs3733/c20/teamS/pathFindingTests/WrittenInstructions_Tests.java
+++ src/test/java/edu/wpi/cs3733/c20/teamS/pathFindingTests/WrittenInstructions_Tests.java
@@ -4,6 +4,8 @@ import edu.wpi.cs3733.c20.teamS.NodeData;
 import edu.wpi.cs3733.c20.teamS.pathfinding.WrittenInstructions;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class WrittenInstructions_Tests {
@@ -16,32 +18,44 @@ public class WrittenInstructions_Tests {
     NodeData nodeFour = new NodeData("4", 200,60, 2, "Hospital", "Room", "longName4", "LN4");
 
     NodeData nodeFive = new NodeData("5", 200,20, 2, "Hospital", "Room", "longName5", "LN5");
+
+    NodeData nodeEleven = new NodeData("11",250, 20, 2, "Hospital", "Room", "longName11", "LN11");
+
+    NodeData nodeTwelve = new NodeData("12", 250,30,2, "Hospital", "Room", "longName12", "LN12");
 ///=========================================================================================================================================
     NodeData nodeSix = new NodeData("6", 300,700,2,"Hospital", "Room", "longName6", "LN6");
 
-    NodeData nodeSeven = new NodeData("7", 300,640,2,"Hospital", "Room", "longName7", "LN7");
+    NodeData nodeSeven = new NodeData("7", 300,840,2,"Hospital", "Room", "longName7", "LN7");
 
-    NodeData nodeEight = new NodeData("8", 300,600,2,"Hospital", "Room", "longName8", "LN8");
+    NodeData nodeEight = new NodeData("8", 300,900,2,"Hospital", "Room", "longName8", "LN8");
 
-    NodeData nodeNine = new NodeData("9", 300,580,2,"Hospital", "Room", "longName9", "LN9");
+    NodeData nodeNine = new NodeData("9", 300,1000,2,"Hospital", "Room", "longName9", "LN9");
 
-    NodeData nodeTen = new NodeData("10", 330,580,2,"Hospital", "Room", "longName10", "LN10");
+    NodeData nodeTen = new NodeData("10", 700,1000,2,"Hospital", "Room", "longName10", "LN10");
 
 
 
-//    @Test
-//    public void turnLeftTest1() {
-//        ArrayList<NodeData> pathtest1 = new ArrayList<NodeData>();
-//        pathtest1.add(nodeOne);
-//        pathtest1.add(nodeTwo);
-//        pathtest1.add(nodeThree);
-//        pathtest1.add(nodeFour);
-//        pathtest1.add(nodeFive);
-//
-//        WrittenInstructions wi = new WrittenInstructions(pathtest1);
-//
-//        assertEquals("Turn Left In 26FT OR 8M", wi.directions());
-//    }
+    @Test
+    public void turnLeftTest1() {
+        ArrayList<NodeData> pathtest1 = new ArrayList<NodeData>();
+        pathtest1.add(nodeOne);
+        pathtest1.add(nodeTwo);
+        pathtest1.add(nodeThree);
+        pathtest1.add(nodeFour);
+        pathtest1.add(nodeFive);
+        pathtest1.add(nodeEleven);
+        pathtest1.add(nodeTwelve);
+
+
+        WrittenInstructions wi = new WrittenInstructions(pathtest1);
+
+      LinkedList<String> realInstructions = new LinkedList<>();
+        realInstructions.add("Go Straight For 5.3012FT or 1.6158M");
+        //realInstructions.add("Go Straight For ")
+
+
+        assertEquals("Turn Left In 26FT OR 8M", wi.directions());
+    }
 
     @Test
     public void turnRightTest1() {
