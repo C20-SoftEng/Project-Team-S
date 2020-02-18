318a88b35dbcc68b169f5581c1bb200f6ec396f3
Newell Clark <newellofthefuture@gmail.com>
2020-02-17 21:18:33 -0500

    Added a RideRequest UI screen that doesn't look like sweaty balls.

--- src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/RideRequestUIController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/RideRequestUIController.java
@@ -2,11 +2,32 @@ package edu.wpi.cs3733.c20.teamS.app.serviceRequests;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import tornadofx.control.DateTimePicker;
 
 public class RideRequestUIController {
     @FXML private JFXTextField riderField;
     @FXML private DateTimePicker pickupTimePicker;
     @FXML private JFXTextField destinationField;
+    @FXML private Label rideKindLabel;
 
+    @FXML private void onLyftClicked() {
+        throw new NotImplementedException();
+    }
+    @FXML private void onFriendClicked() {
+        throw new NotImplementedException();
+    }
+    @FXML private void onShuttleClicked() {
+        throw new NotImplementedException();
+    }
+    @FXML private void onHeliClicked() {
+        throw new NotImplementedException();
+    }
+    @FXML private void onOKClicked() {
+        throw new NotImplementedException();
+    }
+    @FXML private void onCancelClicked() {
+        throw new NotImplementedException();
+    }
 }
