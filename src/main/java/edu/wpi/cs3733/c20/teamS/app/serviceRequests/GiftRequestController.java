63c53b19754b78fa6c3fb2eebab0f3333a45f125
CluelessTimeTraveler <macastro2@wpi.edu>
2020-02-18 20:23:44 -0500

    added data field

---
+++ src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/GiftRequestController.java
@@ -0,0 +1,41 @@
+package edu.wpi.cs3733.c20.teamS.app.serviceRequests;
+
+import edu.wpi.cs3733.c20.teamS.app.DialogEvent;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.DrugServiceRequest;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.GiftServiceRequest;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GiftRequestController {
+
+    private final PublishSubject<DialogEvent<GiftServiceRequest>> dialogCompleted_ = PublishSubject.create();
+    private GiftServiceRequest request = new GiftServiceRequest();
+    private Employee loggedIn;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private TextField locationField;
+
+    @FXML
+    private TextField giftRequestField;
+
+    @FXML
+    private Label serviceName;
+
+
+    public GiftRequestController(Employee employee) {this.loggedIn = employee;}
+
+    public Observable<DialogEvent<GiftServiceRequest>> dialogCompleted() {
+        return dialogCompleted_;
+    }
+}
---
+++ src/main/java/edu/wpi/cs3733/c20/teamS/app/serviceRequests/GiftRequestScreen.java
@@ -0,0 +1,65 @@
+package edu.wpi.cs3733.c20.teamS.app.serviceRequests;
+import edu.wpi.cs3733.c20.teamS.ThrowHelper;
+import edu.wpi.cs3733.c20.teamS.app.DialogEvent;
+import edu.wpi.cs3733.c20.teamS.app.DialogResult;
+import edu.wpi.cs3733.c20.teamS.app.serviceRequests.DrugRequestController;
+import edu.wpi.cs3733.c20.teamS.app.serviceRequests.GiftRequestController;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.DrugServiceRequest;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.GiftServiceRequest;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public final class GiftRequestScreen {
+
+    private final Stage stage;
+    private final Scene scene;
+    private final PublishSubject<DialogEvent<GiftServiceRequest>> subject;
+
+    public GiftRequestScreen(Employee loggedIn){
+        this.stage = new Stage();
+        subject = PublishSubject.create();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/serviceRequests/GiftServiceDialog.fxml"));
+        loader.setControllerFactory(c -> {
+            GiftRequestController controller = new GiftRequestController(loggedIn);
+            controller.dialogCompleted().subscribe(
+                    next -> {
+                        if(next.result() == DialogResult.OK){
+                            //Do database
+                        }
+                        this.stage.close();
+                    }
+            );
+            return controller;
+        });
+
+        try {
+            Parent root = loader.load();
+            scene = new Scene(root);
+        }
+        catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static Observable<DialogEvent<GiftServiceRequest>> showDialog(Employee employee) {
+        if (employee == null) ThrowHelper.illegalNull("employee");
+
+        GiftRequestScreen screen = new GiftRequestScreen(employee);
+        screen.show();
+
+        return screen.subject;
+    }
+
+    private void show() {
+        stage.setScene(scene);
+        stage.show();
+    }
+}
