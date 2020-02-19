adf77465865176315e790ded17f29094c7e59fc2
CarlvinWang7 <46726545+CarlvinWang7@users.noreply.github.com>
2020-02-18 19:33:52 -0500

    Implement ActiveServiceRequestScreen and controller to be able to display columns in tableview

--- src/main/java/edu/wpi/cs3733/c20/teamS/App.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/App.java
@@ -1,16 +1,32 @@
 package edu.wpi.cs3733.c20.teamS;
 
+import edu.wpi.cs3733.c20.teamS.serviceRequests.ActiveServiceRequestScreen;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.RideServiceRequest;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.ServiceRequest;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-
+    ObservableList<ServiceRequest> testSet = FXCollections.observableArrayList();
+    RideServiceRequest testServiceRequest = new RideServiceRequest(1);
+    Employee tom = new Employee(1, "Tom");
+    testServiceRequest.assignTo(tom);
+    testServiceRequest.setMessage("Kobe is on helicopter");
+    testServiceRequest.setLocation("Morgue");
+    testSet.add(testServiceRequest);
+    ActiveServiceRequestScreen testScreen = new ActiveServiceRequestScreen(primaryStage, testSet);
   }
 }
--- src/main/java/edu/wpi/cs3733/c20/teamS/database/DatabaseController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/database/DatabaseController.java
@@ -547,33 +547,8 @@ public class DatabaseController implements DBRepo{
         }
     }
 
-    public void addServiceRequest(ServiceRequest request) {
-        if (request == null) ThrowHelper.illegalNull("request");
 
-        AddServiceVisitor visitor = new AddServiceVisitor();
 
-        //  When ServiceRequest.accept() is called, visit() will be called on the ServiceVisitor. What's more,
-        //  the correct overload of visit() will be called.
-        request.accept(visitor);
-    }
-
-    //  Each time you need to perform a specific operation differently for different subclasses
-    //  of ServiceRequest, you can subclass ServiceVisitor.
-    //  The base ServiceVisitor class must have an abstract method for each subclass of ServiceRequest.
-    private final class AddServiceVisitor extends ServiceVisitor {
-        //  Code that is specific to each particular type of service request should go in
-        //  here. When you call accept() on a service request instance, it'll call visit() and
-        //  pass itself as the argument. Because of the way overload resolution works, the correct overload
-        //  will be called.
-        @Override
-        public void visit(JanitorServiceRequest request) {
-            throw new NotImplementedException();
-        }
-        @Override
-        public void visit(RideServiceRequest request) {
-            throw new NotImplementedException();
-        }
-    }
 
     public void removeNode(String nodeID) {
         String delNode = "DELETE FROM NODES WHERE NODEID = '" + nodeID + "'";
