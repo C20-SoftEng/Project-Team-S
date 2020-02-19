d4aa056111435138b87c0740f0e6cf87f515b843
CarlvinWang7 <46726545+CarlvinWang7@users.noreply.github.com>
2020-02-18 19:46:10 -0500

    Add more serviceRequest to test ActiveServiceRequest Screen but encountered error

--- src/main/java/edu/wpi/cs3733/c20/teamS/App.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/App.java
@@ -1,9 +1,6 @@
 package edu.wpi.cs3733.c20.teamS;
 
-import edu.wpi.cs3733.c20.teamS.serviceRequests.ActiveServiceRequestScreen;
-import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
-import edu.wpi.cs3733.c20.teamS.serviceRequests.RideServiceRequest;
-import edu.wpi.cs3733.c20.teamS.serviceRequests.ServiceRequest;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,11 +19,17 @@ public class App extends Application {
   public void start(Stage primaryStage) throws Exception {
     ObservableList<ServiceRequest> testSet = FXCollections.observableArrayList();
     RideServiceRequest testServiceRequest = new RideServiceRequest(1);
+    DrugServiceRequest testServiceRequest2 = new DrugServiceRequest(2);
     Employee tom = new Employee(1, "Tom");
+    Employee patrickStar = new Employee(2,"PatrickStar");
     testServiceRequest.assignTo(tom);
+    testServiceRequest.assignTo(patrickStar);
     testServiceRequest.setMessage("Kobe is on helicopter");
+    testServiceRequest2.setMessage("Where is SpongeBob");
     testServiceRequest.setLocation("Morgue");
+    testServiceRequest2.setLocation("BikiniBottom");
     testSet.add(testServiceRequest);
+    testSet.add(testServiceRequest2);
     ActiveServiceRequestScreen testScreen = new ActiveServiceRequestScreen(primaryStage, testSet);
   }
 }
