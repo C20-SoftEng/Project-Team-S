33a3a573166e66da6c990a51a2a8174e57fcb275
marc-capobianco <macapobianco@wpi.edu>
2020-02-18 20:44:11 -0500

    SERVICE TECH SERVICE REQUEST

--- src/main/java/edu/wpi/cs3733/c20/teamS/serviceRequests/SelectServiceController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/serviceRequests/SelectServiceController.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.c20.teamS.serviceRequests;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.c20.teamS.app.serviceRequests.DrugRequestScreen;
+import edu.wpi.cs3733.c20.teamS.app.serviceRequests.ServiceTechRequestScreen;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import edu.wpi.cs3733.c20.teamS.app.DialogEvent;
@@ -37,6 +38,14 @@ public class SelectServiceController {
         drugStage.initModality(Modality.WINDOW_MODAL);
 
         DrugRequestScreen.showDialog(loggedIn).subscribe();
+        this.stage.close();
+    }
+
+    @FXML void onServiceTechClicked(ActionEvent event){
+        Stage serviceTechStage = new Stage();
+        serviceTechStage.initModality(Modality.WINDOW_MODAL);
+
+        ServiceTechRequestScreen.showDialog(loggedIn).subscribe();
         this.stage.close();
     }
 
