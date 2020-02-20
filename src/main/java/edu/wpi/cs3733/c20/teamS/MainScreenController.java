0ebc0377dfab08ee4e2be4fee2f8f2f88aa6dc91
Newell Clark <newellofthefuture@gmail.com>
2020-02-20 16:42:22 -0500

    Reordered members.

--- src/main/java/edu/wpi/cs3733/c20/teamS/MainScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MainScreenController.java
@@ -22,7 +22,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 
-
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -42,65 +41,6 @@ public class MainScreenController implements Initializable {
     private boolean flip = true;
     private MapZoomer zoomer;
     private FloorSelector floorSelector;
-
-    public MainScreenController(Stage stage, IPathfinding algorithm){
-        this.algorithm = algorithm;
-        this.stage = stage;
-        tester2 = new PathDisplay(group2, parentVBox, this.algorithm);
-    }
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-        initSearchComboBoxFont();
-        initSearchComboBoxAutoComplete();
-
-        zoomer = new MapZoomer(mapImage, scrollPane);
-        tester2 = new PathDisplay(group2, parentVBox, algorithm);
-        LinkedList<String> instructions = new LinkedList<>();
-
-        initFloorSelector();
-    }
-
-    private void initFloorSelector() {
-        floorSelector = new FloorSelector(
-                upButton, downButton,
-                new Floor(floorButton1, "images/Floors/HospitalFloor1.png"),
-                new Floor(floorButton2, "images/Floors/HospitalFloor2.png"),
-                new Floor(floorButton3, "images/Floors/HospitalFloor3.png"),
-                new Floor(floorButton4, "images/Floors/HospitalFloor4.png"),
-                new Floor(floorButton5, "images/Floors/HospitalFloor5.png")
-        );
-        floorSelector.setCurrent(3);
-    }
-    private void initSearchComboBoxFont() {
-        String fontFamily = searchComboBox.getEditor().getFont().getFamily();
-        Font font = new Font(fontFamily, 18);
-        searchComboBox.getEditor().setFont(font);
-    }
-    private void initSearchComboBoxAutoComplete() {
-        DatabaseController db = new DatabaseController();
-        Set<NodeData> nodes = db.getAllNodes();
-        List<String> dictionary = nodes.stream()
-                .map(node -> node.getLongName() + ", " + node.getNodeID())
-                .collect(Collectors.toList());
-        AutoComplete.start(dictionary, searchComboBox);
-    }
-
-    @FXML private ImageView mapImage;
-    @FXML private ScrollPane scrollPane;
-    @FXML private JFXButton floorButton1;
-    @FXML private JFXButton floorButton2;
-    @FXML private JFXButton floorButton3;
-    @FXML private JFXButton floorButton4;
-    @FXML private JFXButton floorButton5;
-    @FXML private JFXButton downButton;
-    @FXML private JFXButton upButton;
-    @FXML private Label location1;
-    @FXML private VBox parentVBox;
-    @FXML private JFXButton zoomInButton;
-    @FXML private JFXButton zoomOutButton;
-    @FXML private Label location2;
-    @FXML private ComboBox<String> searchComboBox;
 
     private static class Floor {
         public final Image image;
@@ -174,82 +114,47 @@ public class MainScreenController implements Initializable {
         }
     }
 
-    @FXML private void onUpClicked() {
-        floorSelector.setCurrent(floorSelector.current() + 1);
+    public MainScreenController(Stage stage, IPathfinding algorithm){
+        this.algorithm = algorithm;
+        this.stage = stage;
+        tester2 = new PathDisplay(group2, parentVBox, this.algorithm);
     }
-    @FXML private void onDownClicked() {
-        floorSelector.setCurrent(floorSelector.current() - 1);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        initSearchComboBoxFont();
+        initSearchComboBoxAutoComplete();
+
+        zoomer = new MapZoomer(mapImage, scrollPane);
+        tester2 = new PathDisplay(group2, parentVBox, algorithm);
+        LinkedList<String> instructions = new LinkedList<>();
+
+        initFloorSelector();
     }
-    @FXML private void onFloorClicked1() {
-        floorSelector.setCurrent(1);
-    }
-    @FXML private void onFloorClicked2() {
-        floorSelector.setCurrent(2);
-    }
-    @FXML private void onFloorClicked3() {
+
+    private void initFloorSelector() {
+        floorSelector = new FloorSelector(
+                upButton, downButton,
+                new Floor(floorButton1, "images/Floors/HospitalFloor1.png"),
+                new Floor(floorButton2, "images/Floors/HospitalFloor2.png"),
+                new Floor(floorButton3, "images/Floors/HospitalFloor3.png"),
+                new Floor(floorButton4, "images/Floors/HospitalFloor4.png"),
+                new Floor(floorButton5, "images/Floors/HospitalFloor5.png")
+        );
         floorSelector.setCurrent(3);
     }
-    @FXML private void onFloorClicked4() {
-        floorSelector.setCurrent(4);
+    private void initSearchComboBoxFont() {
+        String fontFamily = searchComboBox.getEditor().getFont().getFamily();
+        Font font = new Font(fontFamily, 18);
+        searchComboBox.getEditor().setFont(font);
     }
-    @FXML private void onFloorClicked5() {
-        floorSelector.setCurrent(5);
-    }
-
-    @FXML private void onHelpClicked() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/TutorialScreen.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage window = new Stage();
-            window.initModality(Modality.APPLICATION_MODAL);
-            window.setTitle("Help");
-            window.setScene(new Scene(root1));
-            window.setResizable(false);
-            window.show();
-        } catch (Exception e) {
-            System.out.println("Can't load new window");
-        }
-    }
-    @FXML private void onStaffClicked() {
-        LoginScreen.showDialog(this.stage);
-    }
-    @FXML private void onPathfindClicked() {
-        double currentHval = scrollPane.getHvalue();
-        double currentVval = scrollPane.getVvalue();
-        drawNodesEdges();
-        keepCurrentPosition(currentHval, currentVval, zoomer);
-    }
-    @FXML private void onSwapButtonPressed() {
-        String temp = location2.getText();
-        location2.setText(location1.getText());
-        location1.setText(temp);
-    }
-    @FXML private void onZoomInClicked() {
-        this.zoomer.zoomIn();
-        if (zoomer.getZoomStage() == 3){
-            zoomInButton.setDisable(true);
-        }
-        else{
-            zoomOutButton.setDisable(false);
-            zoomInButton.setDisable(false);
-        }
-    }
-    @FXML private void onZoomOutClicked() {
-        //Node content = scrollPane.getContent();
-        this.zoomer.zoomOut();
-        if (zoomer.getZoomStage() == -2){
-            zoomOutButton.setDisable(true);
-        }
-        else{
-            zoomOutButton.setDisable(false);
-            zoomInButton.setDisable(false);
-        }
-    }
-
-    private void keepCurrentPosition(double Hval, double Vval, MapZoomer zoomer){
-        zoomer.zoomSet();
-        scrollPane.setHvalue(Hval);
-        scrollPane.setVvalue(Vval);
+    private void initSearchComboBoxAutoComplete() {
+        DatabaseController db = new DatabaseController();
+        Set<NodeData> nodes = db.getAllNodes();
+        List<String> dictionary = nodes.stream()
+                .map(node -> node.getLongName() + ", " + node.getNodeID())
+                .collect(Collectors.toList());
+        AutoComplete.start(dictionary, searchComboBox);
     }
 
     public JFXButton getFloorImage2() {
@@ -322,7 +227,6 @@ public class MainScreenController implements Initializable {
         group.getChildren().add(group2);
         scrollPane.setContent(group);
     }
-
     private NodeData findNearestNode(double x, double y) {
         NodeData nearest = new NodeData();
         double distance = 200;
@@ -346,5 +250,98 @@ public class MainScreenController implements Initializable {
             flip = true;
         }
         return nearest;
+    }
+
+    private void keepCurrentPosition(double Hval, double Vval, MapZoomer zoomer){
+        zoomer.zoomSet();
+        scrollPane.setHvalue(Hval);
+        scrollPane.setVvalue(Vval);
+    }
+
+    @FXML private ImageView mapImage;
+    @FXML private ScrollPane scrollPane;
+    @FXML private JFXButton floorButton1;
+    @FXML private JFXButton floorButton2;
+    @FXML private JFXButton floorButton3;
+    @FXML private JFXButton floorButton4;
+    @FXML private JFXButton floorButton5;
+    @FXML private JFXButton downButton;
+    @FXML private JFXButton upButton;
+    @FXML private Label location1;
+    @FXML private VBox parentVBox;
+    @FXML private JFXButton zoomInButton;
+    @FXML private JFXButton zoomOutButton;
+    @FXML private Label location2;
+    @FXML private ComboBox<String> searchComboBox;
+
+    @FXML private void onUpClicked() {
+        floorSelector.setCurrent(floorSelector.current() + 1);
+    }
+    @FXML private void onDownClicked() {
+        floorSelector.setCurrent(floorSelector.current() - 1);
+    }
+    @FXML private void onFloorClicked1() {
+        floorSelector.setCurrent(1);
+    }
+    @FXML private void onFloorClicked2() {
+        floorSelector.setCurrent(2);
+    }
+    @FXML private void onFloorClicked3() {
+        floorSelector.setCurrent(3);
+    }
+    @FXML private void onFloorClicked4() {
+        floorSelector.setCurrent(4);
+    }
+    @FXML private void onFloorClicked5() {
+        floorSelector.setCurrent(5);
+    }
+    @FXML private void onHelpClicked() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/TutorialScreen.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage window = new Stage();
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setTitle("Help");
+            window.setScene(new Scene(root1));
+            window.setResizable(false);
+            window.show();
+        } catch (Exception e) {
+            System.out.println("Can't load new window");
+        }
+    }
+    @FXML private void onStaffClicked() {
+        LoginScreen.showDialog(this.stage);
+    }
+    @FXML private void onPathfindClicked() {
+        double currentHval = scrollPane.getHvalue();
+        double currentVval = scrollPane.getVvalue();
+        drawNodesEdges();
+        keepCurrentPosition(currentHval, currentVval, zoomer);
+    }
+    @FXML private void onSwapButtonPressed() {
+        String temp = location2.getText();
+        location2.setText(location1.getText());
+        location1.setText(temp);
+    }
+    @FXML private void onZoomInClicked() {
+        this.zoomer.zoomIn();
+        if (zoomer.getZoomStage() == 3){
+            zoomInButton.setDisable(true);
+        }
+        else{
+            zoomOutButton.setDisable(false);
+            zoomInButton.setDisable(false);
+        }
+    }
+    @FXML private void onZoomOutClicked() {
+        //Node content = scrollPane.getContent();
+        this.zoomer.zoomOut();
+        if (zoomer.getZoomStage() == -2){
+            zoomOutButton.setDisable(true);
+        }
+        else{
+            zoomOutButton.setDisable(false);
+            zoomInButton.setDisable(false);
+        }
     }
 }
