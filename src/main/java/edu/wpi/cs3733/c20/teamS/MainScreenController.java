3b61b56794e4962c7fb00427447c92d9af2d51fa
Newell Clark <newellofthefuture@gmail.com>
2020-02-20 17:25:36 -0500

    Oops, forgot to delete the commented-out button that was exposed as a property. I'm still laughing, by the way.

--- src/main/java/edu/wpi/cs3733/c20/teamS/MainScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MainScreenController.java
@@ -153,9 +153,6 @@ public class MainScreenController implements Initializable {
         AutoComplete.start(dictionary, searchComboBox);
     }
 
-/*    public JFXButton getFloorImage2() {
-        return floorButton2;
-    }*/
     public void drawNodesEdges() {
         String floor = "0" + floorSelector.current();
         Group group = new Group();
