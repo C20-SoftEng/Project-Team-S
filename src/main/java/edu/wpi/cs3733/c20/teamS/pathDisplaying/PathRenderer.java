0ff786a1037379a2d44e35dc9db50c312ab21f14
Newell Clark <newellofthefuture@gmail.com>
2020-02-22 08:45:38 -0500

    Got path drawing working again.

--- src/main/java/edu/wpi/cs3733/c20/teamS/pathDisplaying/PathRenderer.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/pathDisplaying/PathRenderer.java
@@ -1,21 +1,75 @@
 package edu.wpi.cs3733.c20.teamS.pathDisplaying;
 
 import com.google.common.graph.MutableGraph;
+import edu.wpi.cs3733.c20.teamS.ThrowHelper;
 import edu.wpi.cs3733.c20.teamS.database.NodeData;
 import edu.wpi.cs3733.c20.teamS.pathfinding.IPathfinding;
+import edu.wpi.cs3733.c20.teamS.pathfinding.Path;
+import edu.wpi.cs3733.c20.teamS.utilities.Board;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PathRenderer {
-    private final PathFinderStateMachine pathFinder;
+    private final Group group;
 
-    public PathRenderer(MutableGraph<NodeData> graph, IPathfinding algorithm) {
-        pathFinder = new PathFinderStateMachine(graph, algorithm);
+    public PathRenderer(Group group) {
+        if (group == null) ThrowHelper.illegalNull("group");
+
+        this.group = group;
     }
 
-    public void redraw() {
+    public void draw(Path path, int floor) {
+        if (path == null) ThrowHelper.illegalNull("path");
 
+        group.getChildren().clear();
+
+        List<Line> lines = findCompleteEdges(path.startToFinish()).stream()
+                .filter(edge -> edge.isOnFloor(floor))
+                .map(this::drawEdge)
+                .collect(Collectors.toList());
+        group.getChildren().addAll(lines);
     }
 
-    public void onNodeClicked(NodeData node) {
+    private static final class Edge {
+        public final NodeData start;
+        public final NodeData end;
 
+        public Edge(NodeData start, NodeData end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public boolean isComplete() {
+            return start != null && end != null;
+        }
+        public boolean isOnFloor(int floor) {
+            if (!isComplete())
+                return false;
+
+            return start.getFloor() == floor || end.getFloor() == floor;
+        }
+    }
+
+    private static List<Edge> findCompleteEdges(Iterable<NodeData> nodes) {
+        return Board.asList(nodes).stream()
+                .map(board -> new Edge(board.start(), board.end()))
+                .collect(Collectors.toList());
+    }
+
+    private Line drawEdge(Edge edge) {
+        Line line = new Line();
+        line.setStartX(edge.start.getxCoordinate());
+        line.setStartY(edge.start.getyCoordinate());
+        line.setEndX(edge.end.getxCoordinate());
+        line.setEndY(edge.end.getyCoordinate());
+        line.setStroke(Color.RED);
+        line.setFill(Color.RED.deriveColor(1, 1, 1, 0.5));
+        line.setStrokeWidth(10);
+
+        return line;
     }
 }
