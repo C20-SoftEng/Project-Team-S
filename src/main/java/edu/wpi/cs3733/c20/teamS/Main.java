886f86dbd9a999fc0508c3cd11096ca8f344ba18
Newell Clark <newellofthefuture@gmail.com>
2020-02-23 18:10:13 -0500

    Fixed small merge conflict with Main, and added required dependencies for food request thingy.

--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -1,7 +1,10 @@
 package edu.wpi.cs3733.c20.teamS;
 
+import edu.wpi.cs3733.c20.teamS.Editing.MapEditingScreen;
 import edu.wpi.cs3733.c20.teamS.database.DatabaseController;
 import edu.wpi.cs3733.c20.teamS.pathfinding.AStar;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.AccessLevel;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,17 +15,6 @@ public class Main extends Application {
         DatabaseController dbc = new DatabaseController();
         dbc.importStartUpData();
 
-<<<<<<< HEAD
-    new MainToLoginScreen(primaryStage, new AStar());
-//    MapEditingScreen test = new MapEditingScreen(primaryStage,
-//            new Employee(17, "Bob", AccessLevel.ADMIN));
-
-  }
-
-  public static void main(String[] args) {
-    App.launch();
-  }
-=======
         if (START_ON_ADMIN_SCREEN)
             new MapEditingScreen(primaryStage, new Employee(17, "Bob", AccessLevel.ADMIN));
         else
@@ -32,5 +24,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         App.launch();
     }
->>>>>>> feature_leftClickAdd_rightClickDelete
 }
