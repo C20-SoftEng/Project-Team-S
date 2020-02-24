e255a5b3f2c13724399bb880ad6f568dc2b15bab
Jordan <jstoessel@wpi.edu>
2020-02-23 20:53:18 -0500

    added info to the node edit controller

--- src/main/java/edu/wpi/cs3733/c20/teamS/Editing/NodeEditController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Editing/NodeEditController.java
@@ -1,42 +1,38 @@
 package edu.wpi.cs3733.c20.teamS.Editing;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.subjects.PublishSubject;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.fxml.Initializable;
 
-public class NodeEditController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NodeEditController implements Initializable {
+
+
     @FXML private JFXTextField floorNumber;
     @FXML private JFXTextField buildingName;
     @FXML private JFXTextField fullNodeName;
     @FXML private JFXTextField shortNodeName;
+    @FXML private JFXComboBox<String> nodeType;
     @FXML private void onOKClicked() {
         okClicked.onNext(this);
     }
     @FXML private void onCancelClicked() {
         cancelClicked.onNext(this);
     }
-    private String nodeTypings[] =
-            {
-                    "HALL", "DEPT", "CONF",
-                    "SERV", "RETL", "INFO",
-                    "LABS", "ELEV", "STAI", "EXIT"
-            };
 
-    @FXML private JFXComboBox nodeType = new JFXComboBox();
     private PublishSubject<Object> okClicked = PublishSubject.create();
     private PublishSubject<Object> cancelClicked = PublishSubject.create();
 
     public NodeEditController() {
-        nodeType.getItems().addAll(nodeTypings);
+
+
+
+
     }
 
     public int floorNumber() {
@@ -46,8 +42,7 @@ public class NodeEditController {
         return buildingName.getText();
     }
     public String nodeType() {
-        System.out.println(nodeType.getValue());
-        return nodeType.getValue().toString();
+        return nodeType.getValue();
     }
     public String fullNodeName() {
         return fullNodeName.getText();
@@ -57,13 +52,25 @@ public class NodeEditController {
     }
 
     public Observable<Object> okClicked() {
-        //if(!buildingName().equals("") && !nodeType().equals("") && !fullNodeName().equals("") && !shortNodeName().equals("")){
-        //    return okClicked;
-        //}else return null;
 
-        return okClicked;
+//        if(!buildingName().equals("") && !nodeType().equals("") && !fullNodeName().equals("") && !shortNodeName().equals("")){
+//            return okClicked;
+//        }
+//        else {
+//            return null;
+//        }
+    return okClicked;
     }
     public Observable<Object> cancelClicked() {
         return cancelClicked;
+    }
+
+    public void initialize(URL url, ResourceBundle rb)  {
+
+
+        nodeType.getItems().addAll("HALL", "DEPT", "CONF",
+                "SERV", "RETL", "INFO",
+                "LABS", "ELEV", "STAI", "EXIT");
+
     }
 }
--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -1,7 +1,11 @@
 package edu.wpi.cs3733.c20.teamS;
 
+import edu.wpi.cs3733.c20.teamS.Editing.MapEditingScreen;
+import edu.wpi.cs3733.c20.teamS.Editing.NodeEditScreen;
 import edu.wpi.cs3733.c20.teamS.database.DatabaseController;
 import edu.wpi.cs3733.c20.teamS.pathfinding.AStar;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.AccessLevel;
+import edu.wpi.cs3733.c20.teamS.serviceRequests.Employee;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,10 +14,10 @@ public class Main extends Application {
   public void start(Stage primaryStage) {
     DatabaseController dbc = new DatabaseController();
     dbc.importStartUpData();
-
-    new MainToLoginScreen(primaryStage, new AStar());
-//    MapEditingScreen test = new MapEditingScreen(primaryStage,
-//            new Employee(17, "Bob", AccessLevel.ADMIN));
+   // new NodeEditScreen(primaryStage);
+    //new MainToLoginScreen(primaryStage, new AStar());
+    MapEditingScreen test = new MapEditingScreen(primaryStage,
+            new Employee(17, "Bob", AccessLevel.ADMIN));
 
   }
 
