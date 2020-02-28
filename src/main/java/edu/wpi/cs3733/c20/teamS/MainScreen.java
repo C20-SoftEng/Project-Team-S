7eb68ddf99da9eb3864acb7c3da9b38e5a87d6ea
CluelessTimeTraveler <macastro2@wpi.edu>
2020-02-28 15:52:48 -0500

    jar

---
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MainScreen.java
@@ -0,0 +1,16 @@
+package edu.wpi.cs3733.c20.teamS;
+
+import edu.wpi.cs3733.c20.teamS.utilities.UIWatchPug;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public abstract class MainScreen {
+    public static final UIWatchPug puggy;
+    public static Stage stage;
+
+    static {
+        //Stage stage = new Stage();
+        puggy = new UIWatchPug(new Duration(10000),() -> MainStartScreen.showDialog(stage));
+    }
+}
+
