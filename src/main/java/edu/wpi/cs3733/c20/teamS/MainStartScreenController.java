97d17f1d64eaa15ccf74d330703a64d772eaad83
owencsmith <ocsmith@wpi.edu>
2020-02-28 18:21:30 -0500

    this should not overwrite the thing on devvelopment now

--- src/main/java/edu/wpi/cs3733/c20/teamS/MainStartScreenController.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/MainStartScreenController.java
@@ -125,8 +125,8 @@ public class MainStartScreenController implements Initializable {
         }
 
 
-       // Image image = new Image(String.valueOf(getClass().getResource("/images/" + icon)));
-        Image image = new Image(String.valueOf(getClass().getResource("/images/" + "weatherIcons/ThunderStorm.png")));
+        Image image = new Image(String.valueOf(getClass().getResource("/images/" + icon)));
+        //Image image = new Image(String.valueOf(getClass().getResource("/images/" + "weatherIcons/ThunderStorm.png")));
 
         imageID.setImage(image);
         imageID.setFitHeight(171);
