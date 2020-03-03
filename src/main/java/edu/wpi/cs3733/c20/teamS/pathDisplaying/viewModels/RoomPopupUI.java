6a6873d0c11d4101b39174b51d38a15cb3f480e4
Newell Clark <newellofthefuture@gmail.com>
2020-03-02 20:41:10 -0500

    Added rudimentary room-popup information to the main pathfinding screen. Hopefully Jordan can work his UI magic on it to niceify it.

---
+++ src/main/java/edu/wpi/cs3733/c20/teamS/pathDisplaying/viewModels/RoomPopupUI.java
@@ -0,0 +1,42 @@
+package edu.wpi.cs3733.c20.teamS.pathDisplaying.viewModels;
+
+import edu.wpi.cs3733.c20.teamS.ThrowHelper;
+import edu.wpi.cs3733.c20.teamS.collisionMasks.Room;
+import edu.wpi.cs3733.c20.teamS.utilities.rx.RxAdaptors;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+/**
+ * Class responsible for rendering the popup information on the main pathfinding
+ * screen when you hover over a room.
+ */
+class RoomPopupUI extends Parent {
+    private final Room room;
+    private final VBox vbox = new VBox();
+    private final Label nameLabel = new Label();
+    private final TextArea descriptionTextArea = new TextArea();
+
+    public RoomPopupUI(Room room) {
+        if (room == null) ThrowHelper.illegalNull("room");
+
+        this.room = room;
+        nameLabel.setFont(new Font(nameLabel.getFont().getFamily(), 22));
+        vbox.getChildren().addAll(nameLabel, descriptionTextArea);
+        getChildren().add(vbox);
+
+        updateUI();
+
+        room.nameChanged().map(n -> RxAdaptors.UNIT)
+                .mergeWith(room.descriptionChanged())
+                .mergeWith(room.iconChanged())
+                .subscribe(u -> updateUI());
+    }
+
+    private void updateUI() {
+        nameLabel.setText(room.name());
+        descriptionTextArea.setText("The quick brown fox jumps over the lazy dog.");
+    }
+}
