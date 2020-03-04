6f979ed340dee22aaac8596efe45951acfdf385f
Newell Clark <newellofthefuture@gmail.com>
2020-03-04 00:46:38 -0500

    Commit so I can pull

--- src/main/java/edu/wpi/cs3733/c20/teamS/pathDisplaying/viewModels/RoomPopupUI.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/pathDisplaying/viewModels/RoomPopupUI.java
@@ -6,6 +6,9 @@ import edu.wpi.cs3733.c20.teamS.utilities.rx.RxAdaptors;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
@@ -16,15 +19,23 @@ import javafx.scene.text.Font;
 class RoomPopupUI extends Parent {
     private final Room room;
     private final VBox vbox = new VBox();
+    private final ImageView iconView = new ImageView();
     private final Label nameLabel = new Label();
+    private final HBox hbox = new HBox();
     private final TextArea descriptionTextArea = new TextArea();
 
     public RoomPopupUI(Room room) {
         if (room == null) ThrowHelper.illegalNull("room");
 
         this.room = room;
+        Image image = new Image(room.icon());
+        iconView.setImage(image);
+        iconView.setFitWidth(75);
+        iconView.setFitHeight(75);
         nameLabel.setFont(new Font(nameLabel.getFont().getFamily(), 22));
-        vbox.getChildren().addAll(nameLabel, descriptionTextArea);
+        hbox.getChildren().addAll(iconView, nameLabel);
+
+        vbox.getChildren().addAll(hbox, descriptionTextArea);
         getChildren().add(vbox);
 
         updateUI();
@@ -41,6 +52,7 @@ class RoomPopupUI extends Parent {
 
     private void updateUI() {
         nameLabel.setText(room.name());
+
         descriptionTextArea.setText(room.description());
     }
 }
