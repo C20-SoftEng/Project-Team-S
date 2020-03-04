fbfd08b6d34a3fb374a56208c71633c46fd0bf21
owencsmith <ocsmith@wpi.edu>
2020-03-03 21:50:27 -0500

    splash screen cosmetics

--- src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
+++ src/main/java/edu/wpi/cs3733/c20/teamS/Main.java
@@ -12,7 +12,7 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    private static final StartupScreen START_SCREEN = StartupScreen.MAIN;
+    private static final StartupScreen START_SCREEN = StartupScreen.SPLASH;
 
 
     public void start(Stage primaryStage) {
